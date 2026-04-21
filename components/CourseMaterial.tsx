4eaa2883de76465012f3447ef386f1e186ca74d0
heshan <heshansandakelum2018@gmail.com>
2026-04-21 10:09:48 +0530

    add map function to tabel headers iteration

--- components/CourseMaterial.tsx
+++ components/CourseMaterial.tsx
@@ -1,26 +1,37 @@
 import Table from 'react-bootstrap/Table';
 import { getCourseMaterialData } from '../service/CourseMaterialService';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
 export const CourseMaterial=()=>{
+    const tblHeaders : string[]=[
+        "Material ID",
+        "File Name",
+        "Material Type",
+        "Material",
+        "Upload At",
+        "Course Id"];
+    }
+
+    const[tblData,settblData]=useState<CourseMaterialModel | null> (null)
+
     useEffect(()=>{
         //console.log("Component Loaded....");
         const loadData=async()=>{
             const importedData=await getCourseMaterialData();
             console.log(importedData)
+            settblData(importedData)
         };
         loadData();
-    },)
+    },[])
     return(
         <>
         <Table striped bordered hover>
       <thead>
         <tr>
-          <th>Material Id</th>
-          <th>File Name</th>
-          <th>Material Type</th>
-          <th>Material</th>
-          <th>Upload At</th>
-          <th>Course Id</th>
+          {
+          tblHeaders.map((headings,index)=>(
+            <th key={index}>{headings}</th>
+          ))}
         </tr>
       </thead>
       <tbody>
@@ -36,4 +47,5 @@ export const CourseMaterial=()=>{
       </tbody>
     </Table></>
     );
+    
 }
